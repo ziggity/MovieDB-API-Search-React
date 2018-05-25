c8c436a4b14b1e9f8e93b8b91ebf455ddc97e4a0
zach beecher <zachbeecher@gmail.com>
2018-05-25 09:54:37 -0700

    add input bar and logo, some styling too

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import img from './download.png';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Movie search</h1>
-      </div>
+        <table className="titleBar">
+          <tbody>
+            <tr>
+              <td>
+              <img src={img} width="50" alt="logo" className="logo1" />
+                </td>
+                <td>
+                  <h3>  Movies Search</h3>
+                
+                  </td>
+
+              </tr>
+            </tbody>
+          </table>
+          <input style={{
+            fontSize: 24,
+            display: 'block',
+            width: '99%',
+            paddingTop: 8,
+            paddingBottom: 8,
+            paddingLeft: 16
+          }} placeholder="Enter search term here"/>
+        </div>
     );
   }
 }
