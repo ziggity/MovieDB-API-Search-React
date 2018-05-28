c72836ed67df58c9551d3e2629d3390bc2079ef5
zach beecher <zachbeecher@gmail.com>
2018-05-28 14:06:39 -0700

    completed MVP - can search and find movies, open up movie links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
 import React, { Component } from 'react';
 import './App.css';
 import img from './movielogo.svg';
-import MovieRows from './MovieRows';
+import MovieRow from './MovieRow';
+import $ from 'jquery';
+
 class App extends Component {
   constructor(props){
     super(props)
+    this.state = {}
     console.log('initializer')
-    const movies = [
-      {id: 0, poster_src:'https://cdn.movieweb.com/img.teasers.posters/FIAvHGAE1U3gDI_384_c/Justice-League.jpg', title: 'Avengers: Inifinity war', overview: "As the universe battles for the realm, they face off against Thanos!"},
-      {id: 1, title: 'Avengers: Inifinity war', overview: "As the universe battles for the realm, they face off against Thanos!"}
-    ]
+    // const movies = [
+    //   {id: 0, poster_src:'https://cdn.movieweb.com/img.teasers.posters/FIAvHGAE1U3gDI_384_c/Justice-League.jpg', title: 'Avengers: Inifinity war', overview: "As the universe battles for the realm, they face off against Thanos!"},
+    //   {id: 1, title: 'Avengers: Inifinity war', overview: "As the universe battles for the realm, they face off against Thanos!"}
+    // ]
     
-    var movieRow = []
-    movies.forEach((movie)=>{
-      console.log(movie.title)
-      const movieRows = <MovieRows movie={movie} />
-      movieRow.push(movieRows)
+    // var movieRow = []
+    // movies.forEach((movie)=>{
+    //   console.log(movie.title)
+    //   const movieRows = <MovieRows movie={movie} />
+    //   movieRow.push(movieRows)
+    // })
+    // this.state = {rows: movieRow}
+    this.performSearch()
+  }
+  performSearch(searchTerm){
+    console.log('search')
+    const urlString = `https://api.themoviedb.org/3/search/movie?query=${searchTerm}&api_key=014f86b604a00da321c812939566d32b`
+    $.ajax({
+      url: urlString,
+      success: (searchResults) => {
+        console.log(searchResults)
+        const results = searchResults.results
+        var movieRows = []
+
+        results.forEach((movie) => {
+          console.log(movie)
+          movie.poster_src = 'https://image.tmdb.org/t/p/w185' + movie.poster_path
+          const movieRow = <MovieRow key={movie.id} movie={movie} />
+          movieRows.push(movieRow)
+        })
+        this.setState({rows: movieRows})
+      },
+      error:(xhr, status, err)=>{
+        console.log('failed')
+      }
     })
-    this.state = {rows: movieRow}
+  }
+  searchChangeHandler(event){
+    
+    const boundObject = this
+    const searchTerm = event.target.value //<event className="target value"></event>
+    boundObject.performSearch(searchTerm)
   }
   render() {
     return (
@@ -42,8 +75,8 @@ class App extends Component {
             paddingTop: 8,
             paddingBottom: 8,
             paddingLeft: 16
-          }} placeholder="Enter search term here"/>
-          {this.state.rows}
+          }} onChange={this.searchChangeHandler.bind(this)}placeholder="Enter search term here"/>
+        {this.state.rows}
         </div>
     );
   }
